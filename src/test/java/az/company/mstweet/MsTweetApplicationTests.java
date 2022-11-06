0680e1dbd4cec2a5d7203d742e8c6d487fe380a3
huseynahmadov <t.huseyn96@mail.ru>
2022-11-06 16:01:14 +0400

    liquibase and application.yml configured

---
+++ src/test/java/az/company/mstweet/MsTweetApplicationTests.java
@@ -0,0 +1,13 @@
+package az.company.mstweet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTweetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
